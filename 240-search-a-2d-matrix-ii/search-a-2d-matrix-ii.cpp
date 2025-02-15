f4cd2ce3e0311ededf7a95f975a80c4407075460
sstarunkumar <tarunkumar87328@gmail.com>
2025-02-15 11:54:08 +0530

    Update search-a-2d-matrix-ii.cpp

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    #started iterating from the top right corner of the grid [0][m-1] since big values are placed down and smaller values placed left
+    # iteraing from top left is ambiguous since both(right and down) has bigger values.
     bool searchMatrix(vector<vector<int>>& matrix, int target) 
     {
         int r=0,c=matrix[0].size()-1;
