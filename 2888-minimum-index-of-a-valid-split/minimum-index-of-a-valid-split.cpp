3cbdf3d9104dafb18bf8ffd27ed262fd2ee15ed5
sstarunkumar <tarunkumar87328@gmail.com>
2025-03-27 23:34:25 +0530

    Time: 1189 ms (5.33%) | Memory: 102.8 MB (30.74%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) 
+    {
+        unordered_map<int,int> mp;
+        int n = nums.size();
+        int val,fre;
+        val=0,fre=0;
+        for(int i=0;i<n;i++)
+        {
+            mp[nums[i]] ++;
+            int mpfre = mp[nums[i]];
+            if(mpfre > fre)
+            {
+                val = nums[i];
+                fre = mpfre;
+            }
+        }
+        fre = 0;
+        vector<int> vec(n);
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] == val)
+                fre++;
+            vec[i]=fre;
+            cout<<vec[i]<<" ";
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            //    2           1           3       2           (4-1)/2
+            if( vec[i] > (i+1)/2 && vec[n-1] - vec[i] > (n-i-1)/2 )
+                return i;
+        }
+        return -1;
+    }
+};
