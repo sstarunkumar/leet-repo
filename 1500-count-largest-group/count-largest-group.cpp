3e3fff374959377b92369d17691413a79c403f02
sstarunkumar <tarunkumar87328@gmail.com>
2025-04-23 10:22:11 +0530

    Time: 224 ms (6.10%) | Memory: 7.8 MB (91.19%) - LeetSync

---
+++ 1500-count-largest-group/count-largest-group.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int sum(int n)
+    {
+        int ans=0;
+        while(n>0)
+        {
+            ans+=n%10;
+            n/=10;
+        }
+        cout<<ans<<" ";
+        return ans;
+    }
+    int countLargestGroup(int n) 
+    {
+        vector<int> vec(37,0);
+        for(int i=1;i<=n;i++)
+            vec[sum(i)]++;
+        int maxi = 0,count = 0;
+        for(int i=1;i<=36;i++)
+        {
+            if(vec[i]>maxi)
+            {
+                maxi = vec[i]; 
+                count = 0;
+            }
+            if(vec[i]==maxi)
+                count++;
+        }
+        return count;
+    }
+};
