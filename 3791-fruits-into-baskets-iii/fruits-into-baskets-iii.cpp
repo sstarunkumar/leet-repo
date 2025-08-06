1d7d1d71590fcadc664dab867ba07d6ee6b4bab9
Shiva Sai Tarun Kumar <tarunkumar87328@gmail.com>
2025-08-06 23:42:24 +0530

    Time: 355 ms (34.54%) | Memory: 176 MB (98.89%) - LeetSync

---
+++ 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) 
+    {
+        int n = fruits.size();
+        double s = sqrt(n);
+        int windowsize = s;
+        if( s - windowsize != 0 )
+            windowsize ++;
+        vector<int> windowmax( windowsize );
+        for(int i=0;i<windowsize;i++)
+        {
+            int start = i * windowsize, maxi = 0;
+            for( int j=0;j < windowsize && start + j < n ;j++ )
+                maxi = max( maxi,baskets[ start+j ] );
+            windowmax[i] = maxi;
+        }
+        int ans = n;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<windowsize;j++)
+            {
+                if( fruits[i] <= windowmax[j] )
+                {
+                    ans--;
+                    int start = j * windowsize;
+                    for(int x = 0;x<windowsize && start+x < n;x++)
+                        if( fruits[i] <= baskets[start+x] )
+                        {
+                            baskets[ start+x ] = 0;
+                            break;
+                        }
+
+                    int maxi = 0;
+                    for(int x=0;x<windowsize && start + x < n ; x++)
+                        maxi = max( maxi,baskets[start+x] );
+                    windowmax[j] = maxi;
+            
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
