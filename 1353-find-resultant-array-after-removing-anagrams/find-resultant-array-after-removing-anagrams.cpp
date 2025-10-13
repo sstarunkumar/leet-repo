bb066907490647534804d6b35c874ad3deef49f8
Shiva Sai Tarun Kumar <tarunkumar87328@gmail.com>
2025-10-13 20:02:29 +0530

    Time: 1 ms (58.38%) | Memory: 17.8 MB (24.57%) - LeetSync

---
+++ 1353-find-resultant-array-after-removing-anagrams/find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,32 @@
+class Solution 
+{
+public:
+    bool isAnagram(string &a,string &b)
+    {
+        vector<int> chars(26,0);
+        for(char ch:a)
+            chars[ ch-'a' ]++;
+        for(char ch:b)
+            chars[ ch-'a']--;
+        for(int &i:chars)
+            if(i != 0)
+                return false;
+        return true;
+    }
+    vector<string> removeAnagrams(vector<string>& words) 
+    {
+        int left = 0;
+        int n = words.size();
+        vector<string> ans;
+        for(int i=1;i<n;i++)
+        {
+            if( !isAnagram(words[i], words[left]) )
+            {
+                ans.push_back(words[left]);
+                left = i;
+            }
+        }
+        ans.push_back(words[left]);
+        return ans;
+    }
+};
