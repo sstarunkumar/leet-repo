44089bfe74b5694d8e1dd764bd0da331e46abb76
Shiva Sai Tarun Kumar <tarunkumar87328@gmail.com>
2025-10-25 16:27:23 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (78.60%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -6,10 +6,13 @@ public:
         int x = 1e6;
         for(int i=0;i<n;i++)
             if( nums[i] <= 0 || nums[i] > n)
-                nums[i] = x;
+                nums[i] = x ;
         for(int i=0;i<n;i++)
-            if( abs( nums[i] ) != x )
-                nums[ abs(nums[i]) - 1 ] = - abs( nums[ abs( nums[i] ) - 1 ] ) ;
+        {
+            int k = abs( nums[i] );
+            if(k != x)
+                nums[k-1] = - abs( nums[k-1] ) ; 
+        }
         for(int i=0;i<n;i++)
             if(nums[i] > 0)
                 return i+1;
