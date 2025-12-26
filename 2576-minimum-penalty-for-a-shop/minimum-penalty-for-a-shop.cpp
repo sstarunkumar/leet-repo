10bc8c4d2286147177b51d55d565e960d5a9338d
Shiva Sai Tarun Kumar <tarunkumar87328@gmail.com>
2025-12-26 14:36:36 +0530

    Time: 11 ms (37.38%) | Memory: 17.7 MB (50.82%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -5,26 +5,26 @@ public:
         int n = customers.size();
         vector<int> ones(n+1, 0);
         int c = 0;
-        for(int i=0;i<n;i++)
-        {
-            if(customers[i] == 'N')
-                c++;
-            ones[i+1] = c;
-        }
-        c = 0;
         for(int i=n-1;i>=0;i--)
         {
             if(customers[i] == 'Y')
                 c++;
-            ones[i] += c;
+            ones[i] = c;
         }
+        c = 0;
         int ans = 0, val = INT_MAX;
-        for(int i=0;i<=n;i++)
+        for(int i=0;i<n;i++)
+        {
+            if(customers[i] == 'N')
+                c++;
+            ones[i+1] += c;
             if(ones[i] < val)
             {
                 val = ones[i];
                 ans = i;
             }
+        }
+        ans = ones[n] < val ? n : ans;
         return ans;
     }
 };
