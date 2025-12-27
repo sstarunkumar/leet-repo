a4560fcbc99cc35307fc7ba6882b4923f505101a
Shiva Sai Tarun Kumar <tarunkumar87328@gmail.com>
2025-12-27 10:38:12 +0530

    Time: 72 ms (56.77%) | Memory: 98 MB (98.61%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -2,46 +2,50 @@ class Solution {
 public:
     int mostBooked(int n, vector<vector<int>>& meetings) 
     {
-        sort(meetings.begin(),meetings.end());
-        priority_queue<int,vector<int>,greater<>> rooms;
-        priority_queue<pair<long,long>,vector<pair<long,long>>,greater<>> heap;
+        sort(meetings.begin(), meetings.end());
+        int s = meetings.size();
+        vector<int> count(n, 0);
+
+        priority_queue< int, vector<int>, greater<> > rooms;   
+
         for(int i=0;i<n;i++)
             rooms.push(i);
-        int x = meetings.size();
-        vector<int> fre(n);
-        for(int i=0;i<x;i++)
+
+        priority_queue< pair<long, long> , vector< pair<long, long> > , greater<> > endRoom;
+        for(int i=0;i<s;i++)
         {
-            long s = meetings[i][0];
-            long e = meetings[i][1];
-            while(!heap.empty() && heap.top().first <= s)
+            int start = meetings[i][0], end = meetings[i][1];
+            while( !endRoom.empty() && endRoom.top().first <= start )
             {
-                rooms.push(heap.top().second);
-                heap.pop();
+                rooms.push( endRoom.top().second );
+                endRoom.pop();
             }
-            int t;
-            if(rooms.empty())
+            if( rooms.empty() )
             {
-                long d = e - s;
-                s = heap.top().first;
-                e = s + d;
-                t = heap.top().second;
-                heap.pop();              
+                pair<long, long> p = endRoom.top();
+                endRoom.pop();
+
+                count[p.second]++;
+                endRoom.push({ p.first + end - start , p.second });
             }
             else
             {
-                t = rooms.top();
+                int room = rooms.top();
                 rooms.pop();
+
+                count[room]++;
+                endRoom.push({ end, room });
             }
-            fre[t]++;
-            heap.push({e,t});
         }
-        int ans = 0,sum = 0;
+        int val = 0, ans = 0;
         for(int i=0;i<n;i++)
-            if(fre[i] > sum)
+        {
+            if( count[i] > val )
             {
-                sum = fre[i];
+                val = count[i];
                 ans = i;
-            }    
+            }
+        }
         return ans;
     }
 };
