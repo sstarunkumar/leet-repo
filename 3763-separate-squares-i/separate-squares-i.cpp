a71a700d08becc423792eab01f46a683581730a2
Shiva Sai Tarun Kumar <tarunkumar87328@gmail.com>
2026-01-13 15:57:28 +0530

    Time: 95 ms (66.76%) | Memory: 198.5 MB (89.02%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.cpp
+++ 3763-separate-squares-i/separate-squares-i.cpp
@@ -5,30 +5,26 @@ public:
         double area = 0;
         for(vector<int> & i : squares)
         {
-            int x = i[0], y = i[1], s = i[2];
-            if(y >= k)
-                area += pow(s, 2);
-            else if(y+s > k)
-                area += (y + s - k) * s;
+            if(i[1] >= k)
+                area += pow(i[2], 2);
+            else if(i[1]+i[2] > k)
+                area += (i[1] + i[2] - k) * i[2];
         }
         return area;
     }
     double separateSquares(vector<vector<int>>& squares) 
     {
-        double area = 0;
+        double area = 0, lo = 0, hi = 1e9, ss = 0.00001, mid, up;
         for(vector<int> & i : squares)
             area += pow(i[2], 2);
         area/=2;
 
-        double lo = 0, hi = 1e9, ss = 0.00001;
         while(lo <= hi)
         {
-            double mid = (lo+hi)/2;
-            double up = uparea(squares, mid);
-            if(up <= area)
-                hi = mid - ss;
-            else
-                lo = mid + ss;
+            mid = (lo+hi)/2;
+            up = uparea(squares, mid);
+            if(up <= area)   hi = mid - ss;
+            else             lo = mid + ss;
         }
         return lo;   
     }
