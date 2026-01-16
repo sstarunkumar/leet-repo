0df5428173eccae5a9a00d3d33c1c69a3cbce8a6
Shiva Sai Tarun Kumar <tarunkumar87328@gmail.com>
2026-01-16 11:47:27 +0530

    Time: 891 ms (93.20%) | Memory: 268.5 MB (49.51%) - LeetSync

--- 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -3,49 +3,51 @@ public:
     int mod = 1e9+7;
     int maximizeSquareArea(int m, int n, vector<int>& hFences, vector<int>& vFences) 
     {
-        hFences.push_back(1);
-        hFences.push_back(m);
-
-        vFences.push_back(1);
-        vFences.push_back(n);
-
+        int r = hFences.size(), c = vFences.size();
+        
         sort(hFences.begin(), hFences.end());
         sort(vFences.begin(), vFences.end());
 
-        int r = hFences.size();
-        int c = vFences.size();
+        vector<int> hdis(r+1);
+        hdis[0] = hFences[0] - 1;
+        hdis[r] = m - hFences[r-1];
 
-        vector<int> hdis(r-1);
         for(int i=1;i<r;i++)
-            hdis[i-1] = hFences[i] - hFences[i-1];
+            hdis[i] = hFences[i] - hFences[i-1];
         
         unordered_set<long long> hsums;
-        for(int i=0;i<r-1;i++)
+        long long sum;
+        for(int i=0;i<=r;i++)
         {
-            long long sum = 0;
-            for(int j=i;j<r-1;j++)
+            sum = 0;
+            for(int j=i;j<=r;j++)
             {
                 sum += hdis[j];
                 hsums.insert(sum);
             }
         }
 
-        vector<int> vdis(c-1);
+        vector<int> vdis(c+1);
+        vdis[0] = vFences[0] - 1;
+        vdis[c] = n - vFences[c-1];
+
         for(int i=1;i<c;i++)
-            vdis[i-1] = vFences[i] - vFences[i-1];
+            vdis[i] = vFences[i] - vFences[i-1];
 
         long long ans = -1;
-        for(int i=0;i<c-1;i++)
+        for(int i=0;i<=c;i++)
         {
-            long long sum = 0;
-            for(int j=i;j<c-1;j++)
+            sum = 0;
+            for(int j=i;j<=c;j++)
             {
                 sum += vdis[j];
                 if( hsums.count(sum) )
                     ans = max(ans, sum);
             }
         }
+        if(ans == -1)
+            return -1;
         ans = ans % mod;
-        return ans == -1 ? -1 : (ans * ans) % mod;
+        return (ans * ans) % mod;
     }
 };
