ed9790fa06522d51ffea91ac7a0aa548fced5c89
Shiva Sai Tarun Kumar <tarunkumar87328@gmail.com>
2026-02-12 21:38:01 +0530

    Time: 37 ms (97.87%) | Memory: 12.6 MB (90.55%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.cpp
@@ -10,16 +10,18 @@ public:
             i = *it++ - 97;
         
         int ans = 0;
+        int unique, maxi;
+        int fre, l;
         vector<int> count(26, 0);
         for(int i=0;i<n;i++)
         {
             fill( count.begin(), count.end(), 0 );
-            int unique = 0, maxi = 0;
+            unique = 0, maxi = 0;
             for(int j=i;j<n;j++)
             {
                 count[ vec[j] ]++;
-                int fre = count[ vec[j] ];
-                int l = j-i+1;
+                fre = count[ vec[j] ];
+                l = j-i+1;
                 maxi = max(maxi, fre);
                 if( fre == 1 )
                     unique++;
