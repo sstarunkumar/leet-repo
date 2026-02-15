c13d98300645632695ea7b137508ef062dc5af8e
Shiva Sai Tarun Kumar <tarunkumar87328@gmail.com>
2026-02-15 18:21:24 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (12.56%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string addBinary(string a, string b) 
+    {
+        if(a.size() < b.size())
+            swap(a, b);
+        int aa = a.size(), bb = b.size();
+        
+        int c = 0, r = 0;
+        aa --, bb--;
+        string ans = "";
+        while(aa >= 0 && bb >= 0)
+        {
+            r = a[aa] - '0' + b[bb] - '0' + c ;
+            c = r/2;
+            r = r % 2;
+            ans = char(r + '0') + ans;
+            aa--, bb--;
+        }
+        if(aa < 0 && c)
+            return '1' + ans;
+        while(aa >= 0)
+        {
+            r = a[aa] - '0' + c;
+            c = r/2;
+            r = r%2;
+            ans = char(r + '0') + ans;
+            aa--, bb--;
+        }
+        if(c)
+            return '1' + ans;
+        return ans;
+    }
+};
