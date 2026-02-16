bcacbb10ac40df9f1e937314cd65635caae55f9a
Shiva Sai Tarun Kumar <tarunkumar87328@gmail.com>
2026-02-16 10:25:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (73.18%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -3,7 +3,7 @@ public:
     int reverseBits(int n) 
     {
         int x = 0;
-        for(int i=0;i<31;i++)
+        for(int i=0; i<31; i++)
             if( (n >> i) & 1 )
                 x |= 1 << (31 - i) ;
         return x;
