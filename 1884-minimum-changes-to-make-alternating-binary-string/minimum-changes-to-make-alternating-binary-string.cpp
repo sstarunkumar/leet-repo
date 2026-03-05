47eb9af12efa1c0d0ec613a95234eee2d1c32570
Shiva Sai Tarun Kumar <tarunkumar87328@gmail.com>
2026-03-06 00:59:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (51.31%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -3,22 +3,18 @@ public:
     int minOperations(string s) 
     {
         int n = s.size();
-        int oddones = 0, evenones = 0;
-        int even = (n+1)/2, odd = n/2;
-
-        for(int i=0; i < n; i += 2 )
-            if(s[i] == '1')     evenones++;
-        for(int i = 1; i < n; i += 2)
-            if(s[i] == '1')     oddones++;
-        
-        // ideal places have, 
-        // even = 1    odd = 0
-        // evenzeroes      + oddones
-        // even - evenones + oddones
-
-        // even = 0    odd = 1
-        // evenones + oddzeroes
-        // evenones + odd - oddones
-        return min( even - evenones + oddones, evenones + odd - oddones );
+        int zero = 0, one = 1;
+        int x = 0, y = 0;
+        int bot;
+        for(int i=0;i<n;i++)
+        {
+            bot = s[i] - '0';
+            if(zero != bot)
+                x++;
+            if(one != bot)
+                y++;
+            zero ^= 1, one ^= 1;
+        }
+        return min(x, y);
     }
 };
