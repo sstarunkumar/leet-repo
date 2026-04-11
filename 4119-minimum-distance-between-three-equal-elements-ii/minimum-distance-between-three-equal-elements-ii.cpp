12f88f09c626ad336f6043158a2b4155dc727c56
Shiva Sai Tarun Kumar <tarunkumar87328@gmail.com>
2026-04-11 22:20:18 +0530

    Time: 165 ms (97.75%) | Memory: 225 MB (98.25%) - LeetSync

--- 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.cpp
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.cpp
@@ -3,22 +3,20 @@ public:
     int minimumDistance(vector<int>& nums) 
     {
         int n = nums.size();
-        vector< vector<int> > vec(n, vector<int>(2));
+        vector< pair<int, int> > vec(n);
         for(int i=0;i<n;i++)
-        {
-            vec[i][0] = nums[i];
-            vec[i][1] = i;
-        }    
+            vec[i] = { nums[i], i } ;
+
         sort( vec.begin(), vec.end() );
 
         int ans = INT_MAX;
         for(int i=2;i<n;i++)
         {
-            if(vec[i-2][0] == vec[i-1][0] && vec[i-1][0] == vec[i][0])
+            if( vec[i].first == vec[i-1].first && vec[i-1].first == vec[i-2].first )
             {
-                int x = vec[i][1] - vec[i-1][1] +
-                        vec[i][1] - vec[i-2][1] +
-                        vec[i-1][1] - vec[i-2][1] ;
+                int x = vec[i].second - vec[i-1].second +
+                        vec[i].second - vec[i-2].second +
+                        vec[i-1].second - vec[i-2].second ;
                 ans = min(ans, x);
             }
         }
